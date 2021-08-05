58e52cd332cbffc41cf581aa1af9e1a05a925c61
NATKAMONLAK <paintlpch@gmail.com>
2021-08-05 12:50:23 +0700

    yeah

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       {/* header section */}
       <div className='flex justify-center items-end space-x-2'>
         <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
-        <span className='text-gray-400 italic my-2 text-xl'>by ...</span>
+        <span className='text-gray-400 italic my-2 text-xl'>by Paint</span>
       </div>
 
       {/* todo section */}
